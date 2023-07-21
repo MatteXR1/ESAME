a5eb9fde144967db87111148755cde078e186618
MatteXR1 <135012870+MatteXR1@users.noreply.github.com>
2023-07-21 10:59:05 +0200

    Update index.js

--- Esercitazione/FRONT-END/js/index.js
+++ Esercitazione/FRONT-END/js/index.js
@@ -24,3 +24,42 @@ let typed = new Typed(".multiText", {
   backDelay: 1500,
   startDelay: 1000,
 });
+
+
+//Gestione login form
+
+document.querySelector("form").addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  const codiceCliente = document.querySelector(".codiceCliente").value;
+  const URL = "http://localhost:9001/api/clienti";
+  // Effettuo la chiamata alla mia API utilizzando fetch
+  fetch(URL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ cod_cliente: codiceCliente }), // Invia il codice cliente al server come payload JSON con il nome corretto
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Errore nella risposta della richiesta!");
+      }
+      return response.json(); // Parsa la risposta JSON
+    })
+    .then((data) => {
+      // Verifico se il campo cod_cliente è presente nella risposta
+      if ("cod_cliente" in data) {
+        // Se è presente, il codice cliente è valido
+        window.location.href = "./spettacoli.html";
+      } else {
+        // Se non è presente, il codice cliente non esiste nel database
+        alert(
+          "Codice cliente non valido! Si prega di inserire un codice valido."
+        );
+      }
+    })
+    .catch((error) => {
+      console.error("Errore durante la richiesta:", error);
+    });
+});
