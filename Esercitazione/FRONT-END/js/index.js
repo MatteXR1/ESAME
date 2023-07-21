3c51f9637b9a1512c6545daa415f4fd074ba21d5
Matteo Perrone <matteo.personale1@gmail.com>
2023-07-21 09:38:05 +0200

    fine style inizio js login page

---
+++ Esercitazione/FRONT-END/js/index.js
@@ -0,0 +1,28 @@
+let typed = new Typed(".multiText", {
+  strings: [
+    "Three",
+    "Four",
+    "Five",
+    "Six",
+    "Seven",
+    "Eight",
+    "Nine",
+    "Ten",
+    "Twenty",
+    "Thirty",
+    "Forty",
+    "Fifty",
+    "Sixty",
+    "Seventy",
+    "Eighty",
+    "Ninety",
+    "100",
+  ],
+  loop: true,
+  typeSpeed: 100,
+  backSpeed: 80,
+  backDelay: 1500,
+  startDelay: 1000,
+});
+
+
