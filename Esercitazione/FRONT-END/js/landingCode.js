6eb63e9af600d41552420042a8df6c6f4298e61e
Matteo Perrone <matteo.personale1@gmail.com>
2023-07-24 17:48:29 +0200

    .

--- Esercitazione/FRONT-END/js/landingCode.js
+++ Esercitazione/FRONT-END/js/landingCode.js
@@ -1,7 +1,3 @@
 let codice = document.querySelector(".card-title");
-fetch("http://localhost:9001/api/clienti")
-  .then((data) => data.json())
-  .then((response) => {
-    const ultimoCodCliente = localStorage.getItem("lastUser");
-    codice.textContent = JSON.parse(ultimoCodCliente).cod_cliente;
-  });
+const ultimoCodCliente = localStorage.getItem("lastUser");
+codice.textContent = JSON.parse(ultimoCodCliente).cod_cliente;
--- Esercitazione/FRONT-END/js/spettacoli.js
+++ Esercitazione/FRONT-END/js/spettacoli.js
@@ -99,3 +99,79 @@ logOut.addEventListener("click", RemoveFromLocalStorage);
 function RemoveFromLocalStorage() {
   localStorage.clear();
 }
+
+//cart
+let cartIcon = document.querySelector("#cart-icon");
+let cart = document.querySelector(".cart");
+let closeCart = document.querySelector("#close-cart");
+
+cartIcon.onclick = () => {
+  cart.classList.add("active");
+};
+
+closeCart.onclick = () => {
+  cart.classList.remove("active");
+};
+
+if (document.readyState == "loading") {
+  document.addEventListener("DOMContentLoaded", ready);
+} else {
+  ready();
+}
+
+function ready() {
+  let removeCartButtons = document.getElementsByClassName("cart-remove");
+  for (let i = 0; i < removeCartButtons.length; i++) {
+    let button = removeCartButtons[i];
+    button.addEventListener("click", removeCartItem);
+  }
+  let quantityInputs = document.getElementsByClassName("cart-quantity");
+  for (let i = 0; i < quantityInputs.length; i++) {
+    let input = quantityInputs[i];
+    input.addEventListener("change", quantityChanged);
+  }
+
+  let addcart = document.getElementsByClassName("add-cart");
+  for (let i = 0; i < addcart.length; i++) {
+    let button = addcart[i];
+    button.addEventListener("click", addcartClicked);
+  }
+}
+
+function removeCartItem(event) {
+  let buttonClicked = event.target;
+  buttonClicked.parentElement.remove();
+  updateTotal();
+}
+
+function quantityChanged(event) {
+  let input = event.target;
+  if (isNaN(input.value) || input.value <= 0) {
+    input.value = 1;
+  }
+  updateTotal();
+}
+
+function addcartClicked(event) {
+  let button = event.target;
+  let shopProducts = button.parentElement;
+  let title = shopProducts.get
+}
+
+function updateTotal() {
+  let cartContent = document.getElementsByClassName("cart-content")[0];
+  let cartBoxes = cartContent.getElementsByClassName("cart-box");
+  let total = 0;
+  for (let i = 0; i < cartBoxes.length; i++) {
+    let cartBox = cartBoxes[i];
+    let priceElement = cartBox.getElementsByClassName("cart-price")[0];
+    let quantityElement = cartBox.getElementsByClassName("cart-quantity")[0];
+    let price = parseFloat(priceElement.innerText.replace("€", ""));
+    let quantity = quantityElement.value;
+    total = total + quantity * price;
+
+    total = Math.round(total * 100) / 100;
+
+    document.getElementsByClassName("total-price")[0].innerText = "€" + total;
+  }
+}
